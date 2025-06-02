366a9b3f7fd8cd786f6ea05eb9f463ec1c4c9528
Wesley J. Davis <wjdavis5@discover11.prv.cube>
2025-06-02 16:57:54 -0400

    good find, good size check, good copy

--- src/bin/NCEP_MM.sh
+++ src/bin/NCEP_MM.sh
@@ -8,11 +8,19 @@ yy=$( echo $yyyymm | cut -c 3-4 )
 
 echo $yyyy $yy $mm
 #cd /archive/input/dao_ops/obs/flk/ncep_ana/Grib/ncep_ana/Y2025/M05
-word_count=$( ls -atlr ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* | wc )
-ls ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* > ${yyyymm}_NCEP_files.list
+ls -atlr ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* > ${yyyymm}_NCEP_files.list
+#ls ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* > ${yyyymm}_NCEP_files.list
 #ls gdas1.PGrbF00.2505* | wc
 
 while IFS= read -r line  ; do
   # Process the line here
-  echo "$line"
+  file_size=$( echo "$line" | awk ' { print $5 } ' )
+  if [ $file_size -gt 60000000 ]; then
+	  target_file=$( echo "$line" | awk ' { print $9 } '  )
+	  echo "$target_file"
+	  cp $target_file ../workdir1
+	  ls ../workdir1
+  elif [ $file_size -lt 60000000 ]; then
+	  echo "$line is a bad file."
+  fi
 done < ${yyyymm}_NCEP_files.list
