0fd05b19c864d615ff04b67b16e3b43df62f82aa
Wesley J. Davis <wjdavis5@discover11.prv.cube>
2025-06-02 16:02:52 -0400

    good read of file list

--- src/bin/NCEP_MM.sh
+++ src/bin/NCEP_MM.sh
@@ -8,5 +8,11 @@ yy=$( echo $yyyymm | cut -c 3-4 )
 
 echo $yyyy $yy $mm
 #cd /archive/input/dao_ops/obs/flk/ncep_ana/Grib/ncep_ana/Y2025/M05
-ls -atlr ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* | wc
+word_count=$( ls -atlr ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* | wc )
+ls ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/${NCEP_BASENAME}.${yy}${mm}* > ${yyyymm}_NCEP_files.list
 #ls gdas1.PGrbF00.2505* | wc
+
+while IFS= read -r line  ; do
+  # Process the line here
+  echo "$line"
+done < ${yyyymm}_NCEP_files.list
