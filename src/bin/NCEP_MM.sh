2b8c7a4ce4be97bc378f06478f076d9d37734259
Wesley J. Davis <wjdavis5@discover11.prv.cube>
2025-06-02 15:51:09 -0400

    parse date, find dir

---
+++ src/bin/NCEP_MM.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+source ../config/MM_config.rc
+
+yyyymm=$1
+yyyy=$(echo $yyyymm | cut -c 1-4 )
+mm=$(echo  $yyyymm | cut -c 5-6 )
+yy=$( echo $yyyymm | cut -c 3-4 )
+
+echo $yyyy $yy $mm
+#cd /archive/input/dao_ops/obs/flk/ncep_ana/Grib/ncep_ana/Y2025/M05
+ls -atlr ${NCEP_BASE_DIR}/Y${yyyy}/M${mm}/gdas1.PGrbF00.${yy}${mm}* | wc
+#ls gdas1.PGrbF00.2505* | wc
